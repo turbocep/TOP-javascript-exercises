73423cf137e3ba760a30d8a577ef230dd26c23fb
turbocep <162988238+turbocep@users.noreply.github.com>
2024-05-24 13:10:40 +0200

    complete exercise 2.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+  let finalStr = "";
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < num; i++) {
+      finalStr += str;
+    }
+    return finalStr;
+  }
+  
 };
 
 // Do not edit below this line
