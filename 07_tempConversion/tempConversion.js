aced9415c3037af8b0bf264e06dede0781fa6e4a
turbocep <162988238+turbocep@users.noreply.github.com>
2024-05-27 13:28:11 +0200

    complete exercise 7.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farnheit) {
+  let celsius = (farnheit - 32) * 5 / 9;
+  return Math.round(celsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let farnheit = (celsius / 5 * 9) + 32;
+  return Math.round(farnheit * 10) / 10;
 };
 
 // Do not edit below this line
