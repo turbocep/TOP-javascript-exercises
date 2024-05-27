cccc4e63c7dddea031c6b0895fbb0f65a66cc71d
turbocep <162988238+turbocep@users.noreply.github.com>
2024-05-27 13:06:47 +0200

    complete exercise 5.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(startInt, endInt) {
+  let totalSum = 0;
+  if (startInt < 0 || endInt < 0 || !(typeof(startInt) === "number") || !(typeof(endInt) === "number")) {
+    return "ERROR";
+  } else {
+    if (startInt > endInt) {
+      let temp = startInt;
+      startInt = endInt;
+      endInt = temp;
+    }
+    for (let i = startInt; i <= endInt; i++) {
+      totalSum = totalSum + i;
+    }
+    return totalSum;
+  }
 };
 
 // Do not edit below this line
