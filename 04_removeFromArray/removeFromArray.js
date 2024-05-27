d47c8db9eb599bd7b4e1660ab3482c4b0cc4be68
turbocep <162988238+turbocep@users.noreply.github.com>
2024-05-27 12:31:31 +0200

    complete exercise 4.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removedValue) {
+  let newArray = [];
+  let count = 0;
+  for (item of array) {
+    count = 0;
+    for (remove of removedValue) {
+      if (remove === item) {
+        count++;
+      }
+      }
+    if (count > 0) {
+      continue;
+    } else {
+      newArray.push(item);
+    }
+    }
+  return newArray;
+  }
 
-};
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
